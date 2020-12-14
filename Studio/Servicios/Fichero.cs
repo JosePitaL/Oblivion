192429e746e115b22040b259543520316934ee64
Jose Luis Pita Lorenzo <jose.pita@ozona.es>
2020-12-14 16:57:00 +0100

    Servicio serializar y deserializar fichero (sin probar)

---
+++ Studio/Servicios/Fichero.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Studio.Model;
+using System;
+using System.IO;
+
+namespace Studio.Servicios
+{
+    public static class Fichero
+    {
+        public static Automatismo OpenRobot(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    var file = File.ReadAllText(path);
+                    var automatismo = JsonConvert.DeserializeObject<Automatismo>(file);
+                    return automatismo;
+                }
+                else return null;
+
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        
+        public static void SaveRobot(Automatismo automatismo, string path)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(automatismo)); 
+        }
+    }
+}
